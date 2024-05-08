8fdd26c06151b458af66d15c8bac9cb693e43ee2
dhruvesh <godavale2491@gmail.com>
2024-05-08 18:24:27 +0530

    file created

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello");
+const express = require("express")
+const mongoose = require("mongoose")
+const port = 4000
+
+const app = express()
+
+mongoose.connect("mongodb://127.0.0.1:27017/E-Buy")
+.then(()=>{
+    console.log("Connection Success");
+})
+.catch(()=>{
+    console.log("Connection Failed");
+})
+
+
+app.listen(port,()=>{
+    console.log(`Server is running on ${port}`);
+})
