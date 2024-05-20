b43f4aac4473886895358f8888f7583312b8fbfe
dhruvesh <godavale2491@gmail.com>
2024-05-20 19:14:50 +0530

    route

--- routes/product.js
+++ routes/product.js
@@ -1,16 +1,14 @@
-const express = require("express");
-const router = express.Router();
-const productController = require("../controllers/productController");
+    const express = require("express");
+    const router = express.Router();
+    const productController = require("../controllers/productController");
 
+    router.post("/add-product", productController.createProduct);
+    router.patch("/editProduct/:id", productController.editProduct);
+    router.get("/product-data", productController.getProduct);
+    router.get("/productId/:id", productController.getProductById);
+    router.delete("/deleteProduct/:id", productController.deleteProductById);
+    router.get("/search", productController.searchProducts);
+    router.get("/filter", productController.filterProducts);
+    router.get("/images/:id", productController.getImageById); 
 
-
-router.post("/add-product", productController.createProduct);
-router.patch("/editProduct/:id", productController.editProduct);
-router.get("/product-data", productController.getProduct);
-router.get("/productId/:id", productController.getProductById);
-router.delete("/deleteProduct/:id", productController.deleteProductById);
-router.get("/search", productController.searchProducts);
-router.get("/filter", productController.filterProducts);
-router.get("/images/:id", productController.getImageById); // Endpoint for fetching image
-
-module.exports = router;
+    module.exports = router;
