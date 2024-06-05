2bdea96578dcc0dcd3df2524ba32c9cdca308358
dhruvesh <godavale2491@gmail.com>
2024-06-05 18:49:36 +0530

    orderpage

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -88,4 +88,25 @@ const razorKey = async (req, res) => {
   }
 };
 
-module.exports = { checkout, paymentVerification, getUserOrders, razorKey };
+const deleteOrder = async (req, res) => {
+  try {
+    const { orderId } = req.body;
+
+    if (!orderId) {
+      throw new Error("Order ID is required.");
+    }
+
+    const order = await Order.findOneAndDelete({ order_id: orderId, user_id: req.user._id });
+
+    if (!order) {
+      return res.status(404).json({ error: "Order not found." });
+    }
+
+    res.status(200).json({ success: true, message: "Order deleted successfully." });
+  } catch (error) {
+    console.error("Error deleting order:", error.message, error.stack);
+    res.status(500).send({ error: "Error deleting order", details: error.message });
+  }
+};
+
+module.exports = { checkout, paymentVerification, getUserOrders, razorKey,deleteOrder };
--- routes/payment.js
+++ routes/payment.js
@@ -4,8 +4,9 @@ const paymentController = require("../controllers/paymentController");
 const auth = require("../middleware/auth");
 
 router.post("/checkout", paymentController.checkout);
-router.post("/payment-verification", auth, paymentController.paymentVerification); // Apply auth middleware here
+router.post("/payment-verification", auth, paymentController.paymentVerification);
 router.get("/getkey", paymentController.razorKey);
 router.get("/orders", auth, paymentController.getUserOrders);
+router.delete("/delete-order", auth, paymentController.deleteOrder);
 
 module.exports = router;
